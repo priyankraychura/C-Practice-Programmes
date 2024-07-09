72e39da85729e61b08b907ce23f43d4f52676726
priyankraychura <priyankraychura@gmail.com>
2024-07-09 20:14:16 +0530

    Programe 44 added

--- 44.c
+++ 44.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
 
 int main(){
-    
+    int marks[100], count = 0;
+    float avg, total = 0;
+
+    printf("Enter the maths marks (0 to terminate): ");
+
+    for(int i = 0; ; i++){
+        scanf("%d", &marks[i]);
+
+        if(marks[i] <= 0 ){
+            break;
+        }
+        count++;
+        total = total + marks[i];
+    }
+
+    avg = total / count;
+
+    printf("Average marks in Mathematics: %.2f\n", avg);
 
     return 0;
 }
